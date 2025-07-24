911e58f67330a19d3bbce5a668467bfb24a89c8b
MostafaMemari <m.memari711@gmail.com>
2025-07-24 18:46:48 +0330

    Add TypeScript interfaces for authentication data structures

---
+++ src/types/auth.type.ts
@@ -0,0 +1,42 @@
+export interface signupData {
+  username: string
+  mobile: string
+  password: string
+  confirmPassword: string
+}
+
+export interface signinData {
+  identifier: string
+  password: string
+}
+
+export interface signupStudentData {
+  nationalCode: string
+}
+
+export interface signupCoachData {
+  nationalCode: string
+}
+
+export interface signoutData {
+  refreshToken: string
+}
+
+export interface refreshTokenData {
+  refreshToken: string
+}
+
+export interface forgetPasswordData {
+  mobile: string
+}
+
+export interface resetPasswordData {
+  mobile: string
+  otpCode: string
+  newPassword: string
+}
+
+export interface verifyOtpData {
+  otp: string
+  mobile: string
+}
