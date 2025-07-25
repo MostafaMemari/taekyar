ff3d19aed8eb393e28330a33191d220dc7559ed9
MostafaMemari <m.memari711@gmail.com>
2025-07-25 15:38:42 +0330

    Refactor login components: simplify LoginPage structure, create AuthIllustrationWrapper, and update LoginV1 for improved layout

--- src/app/(blank-layout-pages)/login/page.tsx
+++ src/app/(blank-layout-pages)/login/page.tsx
@@ -2,10 +2,7 @@
 import type { Metadata } from 'next'
 
 // Component Imports
-import Login from '@views/Login'
-
-// Server Action Imports
-import { getServerMode } from '@core/utils/serverHelpers'
+import Login from '@/views/Login'
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -13,10 +10,11 @@ export const metadata: Metadata = {
 }
 
 const LoginPage = async () => {
-  // Vars
-  const mode = await getServerMode()
-
-  return <Login mode={mode} />
+  return (
+    <div className='flex flex-col justify-center items-center min-bs-[100dvh] p-6'>
+      <Login />
+    </div>
+  )
 }
 
 export default LoginPage
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,90 +1,39 @@
 'use client'
 
 // MUI Imports
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { styled, useTheme } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 
 import Link from '@components/Link'
+
+// Component Imports
 import Logo from '@components/layout/shared/Logo'
-import LoginForm from './LoginForm'
 
 // Config Imports
 import themeConfig from '@configs/themeConfig'
 
-// Hook Imports
-import { useImageVariant } from '@core/hooks/useImageVariant'
-import { useSettings } from '@core/hooks/useSettings'
+// Styled Component Imports
+import AuthIllustrationWrapper from './AuthIllustrationWrapper'
+import LoginForm from './LoginForm'
 
-// Type Imports
-import type { SystemMode } from '@core/types'
-import { cn } from '@/utils/cn'
-
-// Styled Custom Components
-const LoginIllustration = styled('img')(({ theme }) => ({
-  zIndex: 2,
-  blockSize: 'auto',
-  maxBlockSize: 680,
-  maxInlineSize: '100%',
-  margin: theme.spacing(12),
-  [theme.breakpoints.down(1536)]: {
-    maxBlockSize: 550
-  },
-  [theme.breakpoints.down('lg')]: {
-    maxBlockSize: 450
-  }
-}))
-
-const MaskImg = styled('img')({
-  blockSize: 'auto',
-  maxBlockSize: 355,
-  inlineSize: '100%',
-  position: 'absolute',
-  insetBlockEnd: 0,
-  zIndex: -1
-})
-
-const LoginV2 = ({ mode }: { mode: SystemMode }) => {
-  // Hooks
-  const { settings } = useSettings()
-  const theme = useTheme()
-  const hidden = useMediaQuery(theme.breakpoints.down('md'))
-  const authBackground = useImageVariant(mode, '/images/pages/auth-mask-light.png', '/images/pages/auth-mask-dark.png')
-
-  const characterIllustration = useImageVariant(
-    mode,
-    '/images/illustrations/auth/v2-login-light.png',
-    '/images/illustrations/auth/v2-login-dark.png',
-    '/images/illustrations/auth/v2-login-light-border.png',
-    '/images/illustrations/auth/v2-login-dark-border.png'
-  )
-
+const LoginV1 = () => {
   return (
-    <div className='flex bs-full justify-center' dir='rtl'>
-      <div className='flex justify-center items-center bs-full bg-backgroundPaper !min-is-full p-6 md:!min-is-[unset] md:p-12 md:is-[480px]'>
-        <Link className='absolute block-start-5 sm:block-start-[33px] inline-end-6 sm:inline-end-[38px]'>
-          <Logo />
-        </Link>
-        <div className='flex flex-col gap-6 is-full sm:is-auto md:is-full sm:max-is-[400px] md:max-is-[unset] mbs-11 sm:mbs-14 md:mbs-0'>
-          <div className='flex flex-col gap-1'>
+    <AuthIllustrationWrapper>
+      <Card className='flex flex-col sm:is-[450px]'>
+        <CardContent className='sm:!p-12'>
+          <Link className='flex justify-center mbe-6'>
+            <Logo />
+          </Link>
+          <div className='flex flex-col gap-1 mbe-6'>
             <Typography variant='h4'>{`به ${themeConfig.templateName} خوش آمدید! 👋🏻`}</Typography>
             <Typography>لطفاً با حساب کاربری خود وارد شوید و ماجراجویی را شروع کنید</Typography>
           </div>
           <LoginForm />
-        </div>
-      </div>
-      <div
-        className={cn('flex bs-full items-center justify-center flex-1 min-bs-[100dvh] relative p-6 max-md:hidden', {
-          'border-ie': settings.skin === 'bordered'
-        })}
-      >
-        <LoginIllustration src={characterIllustration} alt='character-illustration' />
-        {!hidden && (
-          <MaskImg alt='mask' src={authBackground} className={cn({ 'scale-x-[-1]': theme.direction === 'ltr' })} />
-        )}
-      </div>
-    </div>
+        </CardContent>
+      </Card>
+    </AuthIllustrationWrapper>
   )
 }
 
-export default LoginV2
+export default LoginV1
