fc6d9f7d4ca372f32be2be77134b318457bed5eb
MostafaMemari <m.memari711@gmail.com>
2025-08-02 18:06:17 +0330

    refactor: remove unused media query and simplify FixedBottomNav component structure

--- src/components/layout/mobileMenu/FixedBottomNav.tsx
+++ src/components/layout/mobileMenu/FixedBottomNav.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 
 import { useRouter, usePathname } from 'next/navigation'
 
-import { useMediaQuery, BottomNavigation, BottomNavigationAction, Paper } from '@mui/material'
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material'
 
 interface FixedBottomNavProps {
   onMiddleClick: () => void
@@ -13,7 +13,6 @@ interface FixedBottomNavProps {
 const FixedBottomNav = ({ onMiddleClick }: FixedBottomNavProps) => {
   const router = useRouter()
   const pathname = usePathname()
-  const isMobile = useMediaQuery('(max-width:1200px)')
   const [value, setValue] = useState(-1)
 
   useEffect(() => {
@@ -23,47 +22,47 @@ const FixedBottomNav = ({ onMiddleClick }: FixedBottomNavProps) => {
     else setValue(-1)
   }, [pathname])
 
-  if (!isMobile) return null
-
   return (
-    <Paper
-      elevation={8}
-      sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex: 100
-      }}
-    >
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(_, newValue) => {
-          setValue(newValue)
-
-          switch (newValue) {
-            case 0:
-              router.push('/dashboard')
-              break
-            case 1:
-              onMiddleClick()
-              break
-            case 2:
-              router.push('/support')
-              break
-            case 3:
-              router.push('/account')
-              break
-          }
+    <div className='block md:hidden'>
+      <Paper
+        elevation={8}
+        sx={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: 100
         }}
       >
-        <BottomNavigationAction label='داشبورد' icon={<i className='tabler tabler-layout-dashboard text-[20px]' />} />
-        <BottomNavigationAction label='میزکار' icon={<i className='tabler tabler-layout-grid text-[20px]' />} />
-        <BottomNavigationAction label='پشتیبانی' icon={<i className='tabler tabler-help text-[20px]' />} />
-        <BottomNavigationAction label='حساب کاربری' icon={<i className='tabler tabler-user text-[20px]' />} />
-      </BottomNavigation>
-    </Paper>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(_, newValue) => {
+            setValue(newValue)
+
+            switch (newValue) {
+              case 0:
+                router.push('/dashboard')
+                break
+              case 1:
+                onMiddleClick()
+                break
+              case 2:
+                router.push('/support')
+                break
+              case 3:
+                router.push('/account')
+                break
+            }
+          }}
+        >
+          <BottomNavigationAction label='داشبورد' icon={<i className='tabler tabler-layout-dashboard text-[20px]' />} />
+          <BottomNavigationAction label='میزکار' icon={<i className='tabler tabler-layout-grid text-[20px]' />} />
+          <BottomNavigationAction label='پشتیبانی' icon={<i className='tabler tabler-help text-[20px]' />} />
+          <BottomNavigationAction label='حساب کاربری' icon={<i className='tabler tabler-user text-[20px]' />} />
+        </BottomNavigation>
+      </Paper>
+    </div>
   )
 }
 
